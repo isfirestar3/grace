86b069589561d030bb7d2ac98a514e0cb2575aed
likunhui <likunhui@gzrobot.com>
2018-07-03 14:18:49 +0800

    修正：启动是发生错误，进程退出时错误日志没输出.

--- agvinfo/agvinfo_server/agvinfo_main.cpp
+++ agvinfo/agvinfo_server/agvinfo_main.cpp
@@ -18,23 +18,33 @@ int single_hanler(int signum)
 
 int main(int argc, char **argv)
 {
+	int ret_value = 0;
 	static const char startup_message[] = POSIX__EOL
 		"****************************************************************************"POSIX__EOL
 		"*                            application startup                           *"POSIX__EOL
 		"****************************************************************************";
-	loinfo(MODULE_NAME) << startup_message;
+	log__write(MODULE_NAME, kLogLevel_Info, kLogTarget_Stdout | kLogTarget_Filesystem, startup_message);
+	do {
+		// 命令行检查参数 
+		if (check_argv(argc, argv) < 0) {
+			ret_value = 1;
+			break;
+		}
 
-	// 命令行检查参数 
-	if (check_argv(argc, argv) < 0) {
-		return 1;
-	}
-
-	// 监听、运行 
-	if (nsp::toolkit::singleton<tcp_manager>::instance()->tcp_listen(p_run.inet, p_run.port)) {
-		return 2;
-	}
-	if (nsp::toolkit::singleton<udp_manager>::instance()->udp_listen(p_run.inet, p_run.port)) {
-		return 3;
+		// 监听、运行 
+		if (nsp::toolkit::singleton<tcp_manager>::instance()->tcp_listen(p_run.inet, p_run.port)) {
+			ret_value = 2;
+			break;
+		}
+		if (nsp::toolkit::singleton<udp_manager>::instance()->udp_listen(p_run.inet, p_run.port)) {
+			ret_value = 3;
+			break;
+		}
+	} while (0);
+	if (ret_value) {
+		std::this_thread::sleep_for(std::chrono::milliseconds(500));
+		log__write(MODULE_NAME, kLogLevel_Info, kLogTarget_Stdout | kLogTarget_Filesystem, "quit.");
+		return ret_value;
 	}
 
 	// 等待退出
