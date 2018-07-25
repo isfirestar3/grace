96fa0ee3a2c6cb57caa4ab1e21272d9d5172db40
anderson <250603123@qq.com>
2018-07-25 16:31:13 +0800

    update agvinfo test

--- agvinfo/agvinfo_test/agvinfo_test.cpp
+++ agvinfo/agvinfo_test/agvinfo_test.cpp
@@ -1,9 +1,44 @@
 #include "libagvinfo.h"
+#include "log.h"
+#include "endpoint.h"
+#include "os_util.hpp"
 
-#include <unistd.h>
+#define CONVERT_STATUS_STRING(status) ((status) == (1) ? ("online") : ("offline"))
 
-int main(int argc, char **atgv) {
-	connect_agvinfo_server("127.0.0.1", 9100);
-	while(1) sleep(1);
-	return 0;
+void view_agvs() {
+    struct agv_info *agvs;
+    if (load_agvinfo(&agvs, LAM_Server) >= 0) {
+        loinfo("agvinfo_test") << "agv info changed.";
+        struct agv_info *cursor = agvs;
+        while (cursor) {
+            loinfo("agvinfo_test") << "vehicle:[" << cursor->vhid << "," << CONVERT_STATUS_STRING(cursor->status) << "] IP is:" << cursor->inet << " MAC is:" << cursor->hwaddr;
+            cursor = cursor->next;
+        }
+    }
+}
+
+int main(int argc, char **argv) {
+    if (argc < 2) {
+        loerror("agvinfo_test") << "Syntax: agvinfo_test arget-host-endpoint";
+        return 1;
+    }
+
+    nsp::tcpip::endpoint ep;
+    if (nsp::tcpip::endpoint::build(argv[1], ep) < 0) {
+        loerror("agvinfo_test") << "Syntax: agvinfo_test target-host-endpoint";
+        return 1;
+    }
+
+    if (connect_agvinfo_server(ep.ipv4(), ep.port()) < 0) {
+        loerror("agvinfo_test") << "failed connect agvinfo server " << ep.to_string();
+        return 1;
+    }
+
+    about_something_changed([](){
+        view_agvs();
+    });
+
+    view_agvs();
+    nsp::os::pshang();
+    return 0;
 }
